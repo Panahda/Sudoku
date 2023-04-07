ad6f481e6d13a70c70e2aaa50d5c601f9e9ecc65
notbouncee <vinceong2001@gmail.com>
2023-04-08 01:30:52 +0800

    added startingscreen

---
+++ Sudoku/StartingScreen.java
@@ -0,0 +1,68 @@
+package Sudoku;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class StartingScreen extends JFrame{
+    
+    private static final long serialVersionUID = 1L;
+	JFrame window;
+    Container con;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton;
+    
+    public static void main(String[] args){
+    	SwingUtilities.invokeLater(new Runnable(){
+    	public void run() {
+        new StartingScreen();
+    	}
+    	});
+    }
+
+    public StartingScreen(){
+        window = new JFrame();
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(new BorderLayout());
+        window.setVisible(true);
+        con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100,100,600,150);
+        titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("Sudoku");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+        
+        startButtonPanel = new JPanel(); 
+        startButtonPanel.setBounds(100,100,100,100);
+        startButtonPanel.setBackground(Color.black);
+        
+        startButton = new JButton("Start!");
+        startButton.setBackground(Color.white);
+        startButton.setForeground(Color.black);
+        startButton.setFont(normalFont);
+        
+        
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+        
+        con.add(titleNamePanel, BorderLayout.CENTER);
+        con.add(startButtonPanel,BorderLayout.SOUTH); 
+        
+        StartBtnInputListener listener = new StartBtnInputListener();
+        startButton.addActionListener(listener);
+    }
+        private class StartBtnInputListener implements ActionListener {
+        	@Override
+        	public void actionPerformed(ActionEvent evt) {
+        		new SudokuMain();
+        		window.dispose();
+        	}
+        }
+
+}
